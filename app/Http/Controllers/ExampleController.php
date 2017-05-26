73c5308d6d347c08a6a2c1a953a17920e78b3161
maxshabl <maxshabl@yandex.ru>
2017-05-26 17:33:45 +0300

    odt parsing was made

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -29,77 +29,32 @@ class ExampleController extends Controller
 
     public function odtParser(Request $request)
     {
+        
 
-        $zip = new \ZipArchive;
-        $zip->open(base_path().'/runtime/filename.odt');
-        $zip->deleteName('content.xml');
-        $zip->extractTo(base_path().'/runtime/filename');
-
-        $zip->addFile(base_path().'/runtime/filename/content.xml');
-        $zip->close();
-        $req = $request;
-       /* if($request->isMethod('post')){
-
+        if($request->isMethod('post')){
             if($request->hasFile('odt')) {
                 $file = $request->file('odt');
                 $file->move(base_path().'/runtime','filename.odt');
-                $path = base_path().'/runtime'.'/'.'filename.odt';
                 $zip = new \ZipArchive;
-                $zip->open($path);
-                $zip->extractTo(base_path().'/runtime/filename');
+                $zip->open(base_path().'/runtime/filename.odt');
+                $content = $zip->getFromName('content.xml');
+                $zip->deleteName('content.xml');
+                $contentPath = base_path().'/runtime/content.xml';
+                $doc = new \DOMDocument();
+                $res = $doc->loadXML($content);
+                $spans = $doc->getElementsByTagName('span');
+                $inVarible = false;
+                foreach ($spans as $item) {
+                    $arr['span'][] = $item;
+                        $item->nodeValue = 'dfgdfgdfgdfg ';
+
+                }
+                $doc->saveXML();
+                $doc->save($contentPath);
+                $zip->addFile($contentPath, 'content.xml');
                 $zip->close();
-                $path2 = base_path().'/runtime/filename/content.xml';
-                $xml = \simplexml_load_file($path2);
-                //$xml = simplexml_load_file($path2);
-                $a = [];
             }
-        }*/
-        /*$path2 = base_path().'/runtime/filename/content.xml';
-        /*$b = file_get_contents($path2);
-        $xml = new \SimpleXMLElement(file_get_contents($path2));
-        $ns = $xml->getNamespaces(true);
-        $xml->registerXPathNamespace('office', $ns['office']);
-        $texts = $xml->xpath('/office:document-content/office:body/office:text');
-        foreach ($texts as $text){
-            $f = $text->asXML();
-        }*/
-        //$d= $sel[0]->asXML();
-        $a = [];
-
-        /*$doc = new \DOMDocument();
-        $res = $doc->loadXML(file_get_contents($path2));
-        $tags = $doc->getElementsByTagName('span');
-        $var = false;
-        foreach ($tags as $tag){
-
-            /*if($tag->nodeValue === '{{') {
-                $tag->nodeValue = '';
-                $var = true;
-                continue;
-            }
-            if($var){
-                $tag->nodeValue = 'Значение';
-            }
-            if($tag->nodeValue === '}}') {
-                $tag->nodeValue = '';
-                $var = true;
-                continue;
-            }*/
-           /* $span[] = $tag->nodeValue;
-
         }
-        $tags = $doc->getElementsByTagName('p');
-
-        foreach ($tags as $tag){
-            $p[] = $tag->nodeValue;
-
-        }
-        $doc->saveXML();
-        $doc->save($path2);
-
-        $p = [];*/
-        $p = [];
-
     }
     
     //
